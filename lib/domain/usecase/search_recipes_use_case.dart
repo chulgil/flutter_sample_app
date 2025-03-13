938126dc8ba8159e29e3a51ed4ca00c83f6ed666
CGLee <chulgil.lee@riman.com>
2025-03-13 13:40:39 +0900

    Feat: Add search filter #2

--- lib/domain/usecase/search_recipes_use_case.dart
+++ lib/domain/usecase/search_recipes_use_case.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_sample_app/domain/data_source/local_storage.dart';
+import 'package:flutter_sample_app/domain/filter/filter_state.dart';
 import 'package:flutter_sample_app/domain/model/recipe.dart';
 import 'package:flutter_sample_app/domain/repository/recipe_repository.dart';
 
@@ -12,10 +13,25 @@ class SearchRecipesUseCase {
   }) : _recipeRepository = recipeRepository,
        _localStorage = localStorage;
 
-  Future<List<Recipe>> execute(String query) async {
-    final results = (await _recipeRepository.getRecipes()).where(
-      (e) => e.name.toLowerCase().contains(query.toLowerCase()),
-    );
+  Future<List<Recipe>> execute(String query, FilterState filterState) async {
+    final results = (await _recipeRepository.getRecipes())
+        .where((e) => e.name.toLowerCase().contains(query.toLowerCase()))
+        .where((e) {
+          if (filterState.time == 'All') {
+            return true;
+          } else {
+            return filterState.time == e.time;
+          }
+        })
+        .where((e) => e.rating >= filterState.rate)
+        .where((e) {
+          if (filterState.category == 'All') {
+            return true;
+          } else {
+            return filterState.category == e.category;
+          }
+        })
+    ;
 
     _localStorage.save({'recipes': results.map((e) => e.toJson())});
     return results.toList();
